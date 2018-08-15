4c638c1c1e7cdf99cd4b0a20173e0445d090b765
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-15 23:27:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,8 @@
 def line(deli_line)
   if deli_line.count < 1 
   puts  "The line is currently empty."
+else
+  deli_line.each_with_index do |each_person, position|
+    deli_line << "#{position+1}. #{each_person}"
   end
 end
